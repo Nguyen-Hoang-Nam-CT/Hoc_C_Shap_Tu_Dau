14bbfe2fffa98dcbdbd5ad7042005da5b6669882
your name <colammoicoan10@gmail.com>
2021-08-30 21:03:16 +0700

    Thêm bài vẻ hình chữ nhật có màu nhé :)) đỉnh

---
+++ VeHinhChuNhat/VeHinhChuNhat/Program.cs
@@ -0,0 +1,30 @@
+﻿//lần đầu tiên code bài kiểu này :))
+using System;
+
+namespace VeHinhChuNhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dai = 50;
+            int rong = 10;
+            for(int i = 0 ; i < rong ; i++)
+                for(int j = 0 ; j < dai ; j++)
+                    if (i == 0 || j == 0 || j == (dai - 1) || i == (rong - 1))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("*");
+                        if (j == (dai - 1))
+                            Console.Write("\n");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("-");
+                    }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+        }
+    }
+}
